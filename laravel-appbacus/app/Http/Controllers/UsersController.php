d0d6543a2ae5309c75761b0f28e632862eb80444
Chucho <bahimer8080@gmail.com>
2020-03-07 12:19:14 -0400

    crud users api

---
+++ laravel-appbacus/app/Http/Controllers/UsersController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->role_id = $request->input("role_id");
+        $user->name = $request->input("name");
+        $user->email = $request->input("email");
+        $user->password = Hash::make($request->input("password")) ;
+
+        if($user->save()){
+            return [" response " => "succesfully"];
+        }
+        else {
+            return [" response " => "error"];
+        }
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $users = User::where("id",$id)->get();
+        return $users;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::where("id",$id)->get();
+        $user[0]->role_id = $request->input("role_id");
+        $user[0]->name = $request->input("name");
+        $user[0]->email = $request->input("email");
+
+        if($user[0]->save()){
+            return [" response " => "succesfully"];
+        }
+        else {
+            return [" response " => "error"];
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if( $user = User::where("id",$id)->delete() ){
+            return [" response " => "succesfully"];
+        }
+        else {
+            return [" response " => "error"];
+        }
+    }
+}
