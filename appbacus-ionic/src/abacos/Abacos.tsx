fbddc7e03a16aa5659e18ab3d5c87791fdb7e58d
puntozap <puntozap@gmail.com>
2020-03-15 15:38:29 -0400

    uploading ionic project pwa

---
+++ appbacus-ionic/src/abacos/Abacos.tsx
@@ -0,0 +1,108 @@
+import { IonButtons, IonItemDivider, IonList, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonItemSliding, IonItem, IonLabel, IonItemOptions, IonItemOption, IonIcon, IonNote, IonGrid, IonRow, IonCol, IonRange } from '@ionic/react';
+import React, { useState } from 'react';
+import { sunny } from 'ionicons/icons';
+import { RouteComponentProps } from 'react-router';
+import x from '../abacus.json';
+import ExploreContainer from '../components/ExploreContainer';
+import './Abacos.css';
+import { isNumber } from 'util';
+// console.log(x)
+
+
+const Page: React.FC<RouteComponentProps<{ name: string; }>> = ({ match }) => {
+
+  const [value, setValue] = useState(10);
+  let [value2, setValue2] = useState(0);
+  const [y] = useState(x) as any;
+
+  const [rangeValue, setRangeValue] = useState<{
+    lower: number;
+    upper: number;
+  }>({ lower: 0, upper: 0 });
+  // function hola(){
+  //   console.log("hola");
+  // }
+  function hola(value: any, valuex: any) {
+    
+    let note = y[value][value2];
+    let unapproved = note < 10 ? "😔 Reprobado" : note >= 10 && note <= 12 ? '😃 Aprobado' : note >= 13 && note <= 16 ? '😁 Aprobado' : note == 17 ? '😆 Aprobado' : note == 18 ? '😅 Aprobado' : '😜 Aprobado';
+    let classes = note < 10 ? "unapproved" : note >= 10 && note <= 12 ? 'approved' : note >= 13 && note <= 16 ? 'approved-2' : note == 17 ? 'approved-3' : note == 18 ? 'approved-4' : 'approved-5';
+    let message = note < 10
+      ? "Debes ir a reparación 😭 así que ¡A estudiar! No mereces estar celebrando. Ya veremos que pasa"
+      : 
+      note >= 10 && note <= 12 ? 'Celebra con una cerveza🍻 pero sabes que no lo mereces' 
+      : 
+      note >= 13 && note <= 16 ? "Celebra con una cerveza🍻 lo mereces por ser parte del promedio, sin embargo, esto puede mejorar" 
+      : 
+      note == 17 ? "Vaya! Vaya!, nada mal. Celebra con una cerveza🍻🕺🏿💃🏽" 
+      : 
+      note == 18 ? "Excelente. 18 no lo saca cualquiera. Celebra con dos cervezas🍻🍻🕺🏿💃🏽 bebe con precaucion, tampoco es para tanto" 
+      : 
+      "Asi es que debe ser. llegar eximido siempre ¡Carajo! Celebra con tres cerveza🍻🍻🍻🕺🏿💃🏽 bebe con precaucion";
+    // let classes="";
+    return (
+      <IonRow>
+        <IonCol size="12" className={classes}>
+          <IonLabel><span className="font-3em">{note} puntos</span> </IonLabel>
+          <br />
+          <IonLabel className="font-2em">{unapproved}</IonLabel>
+          <br />
+          <IonLabel>{message}<br/>{'o simplemente haz lo que desees.'}</IonLabel>
+        </IonCol>
+      </IonRow>
+    );
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>🎓Appbacus</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="12" className="text-center">
+              <h3>¿Cuál es mi nota previa? 🤔</h3>
+              <IonRange pin={true} min={10} max={20} value={value} onIonChange={e => setValue(e.detail.value as number)} />
+
+              <IonRow>
+                <IonCol size="12" className={value<10?"unapproved": value >= 10 && value <= 12?"approved":value >= 13 && value <= 16?"approved-2":value == 17?"approved-3":value == 18?"approved-4":"approved-5"}>
+                  <IonLabel>Llevo <span className="font-3em">{value}</span> puntos nota previa</IonLabel>
+                  <br />
+                  <IonLabel className="font-3em">{y[value]['percentage'] == 40 ? '😃' : y[value]['percentage'] == 60 ? '😁' : y[value]['percentage'] == 70 ? '😆' : y[value]['percentage'] == 80 ? '😅' : '😜'}</IonLabel>
+                </IonCol>
+              </IonRow>
+
+            </IonCol>
+            <IonCol size="12" className="text-center">
+              <h3>¿Cuál es o será mi nota final? 🤔</h3>
+              <IonRange pin={true} min={0} max={20} value={value2} onIonChange={e => setValue2(e.detail.value as number)} />
+              <IonRow>
+                <IonCol size="12" className={value2<10?"unapproved": value2 >= 10 && value2 <= 12?"approved":value2 >= 13 && value2 <= 16?"approved-2":value2 == 17?"approved-3":value2 == 18?"approved-4":"approved-5"}>
+                  <IonLabel>Saque <span className="font-3em">{value2}</span> puntos examen final</IonLabel>
+                  <br />
+                  <IonLabel className="font-3em {value2<10?'unapproved':''}">{value2 < 10 ? '😔' : y[value2]['percentage'] == 40 ? '😃' : y[value2]['percentage'] == 60 ? '😁' : y[value2]['percentage'] == 70 ? '😆' : y[value2]['percentage'] == 80 ? '😅' : '😜'}</IonLabel>
+                </IonCol>
+              </IonRow>
+            </IonCol>
+            <IonCol size="12" className="text-center">
+              <h3>¿Cuál es o será la definitiva? 🤔</h3>
+              {hola(value, value2) as any}
+            </IonCol>
+          </IonRow>
+
+        </IonGrid>
+      </IonContent>
+
+
+
+    </IonPage>
+  );
+};
+
+export default Page;
---
+++ appbacus-ionic/src/components/Menu.tsx
@@ -0,0 +1,71 @@
+import {
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonNote,
+} from '@ionic/react';
+import React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { archiveOutline, archiveSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp,personCircleOutline, bugOutline,trashOutline,calculatorOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+import './Menu.css';
+
+interface MenuProps extends RouteComponentProps {
+  selectedPage: string;
+}
+
+interface AppPage {
+  url: string;
+  iosIcon: string;
+  mdIcon: string;
+  title: string;
+}
+
+const appPages: AppPage[] = [
+  {
+    title: 'Abaco',
+    url: '/abacos/Abaco',
+    iosIcon: calculatorOutline,
+    mdIcon: calculatorOutline
+  },
+  {
+    title: 'Datos',
+    url: '/page/Datos',
+    iosIcon: personCircleOutline,
+    mdIcon: personCircleOutline
+  }
+];
+
+const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
+const Menu: React.FunctionComponent<MenuProps> = ({ selectedPage }) => {
+  console.log(selectedPage);
+  return (
+    <IonMenu contentId="main" type="overlay">
+      <IonContent>
+        <IonList id="inbox-list">
+          <IonListHeader>Inbox</IonListHeader>
+          <IonNote>hi@ionicframework.com</IonNote>
+          {appPages.map((appPage, index) => {
+            return (
+              <IonMenuToggle key={index} autoHide={false}>
+                <IonItem className={selectedPage === appPage.title ||appPage.title=='' ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
+                  <IonIcon slot="start" icon={appPage.iosIcon} />
+                  <IonLabel>{appPage.title}</IonLabel>
+                </IonItem>
+              </IonMenuToggle>
+            );
+          })}
+        </IonList>
+
+        
+      </IonContent>
+    </IonMenu>
+  );
+};
+
+export default withRouter(Menu);
