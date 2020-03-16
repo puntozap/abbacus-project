02738143c84243d256c2dc8e126c64ba3cfcb61c
puntozap <puntozap@gmail.com>
2020-03-16 00:24:09 -0400

    uploading send massive emails

--- laravel-appbacus/app/Http/Controllers/NewslettersController.php
+++ laravel-appbacus/app/Http/Controllers/NewslettersController.php
@@ -15,6 +15,10 @@ use TCG\Voyager\Events\BreadDataUpdated;
 use TCG\Voyager\Events\BreadImagesDeleted;
 use TCG\Voyager\Facades\Voyager;
 use TCG\Voyager\Http\Controllers\Traits\BreadRelationshipParser;
+use App\Mail\SendNewsletter;
+use Illuminate\Support\Facades\Mail;
+use App\Newsletter;
+
 
 class NewslettersController extends \TCG\Voyager\Http\Controllers\Controller
 {
@@ -395,33 +399,45 @@ class NewslettersController extends \TCG\Voyager\Http\Controllers\Controller
      */
     public function store(Request $request)
     {
+        // dd($request);
         $slug = $this->getSlug($request);
 
-        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+        // $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
 
-        // Check permission
-        $this->authorize('add', app($dataType->model_name));
+        // // Check permission
+        // $this->authorize('add', app($dataType->model_name));
 
-        // Validate fields with ajax
-        $val = $this->validateBread($request->all(), $dataType->addRows)->validate();
-        $data = $this->insertUpdateData($request, $slug, $dataType->addRows, new $dataType->model_name());
+        // // Validate fields with ajax
+        // $val = $this->validateBread($request->all(), $dataType->addRows)->validate();
+        // $data = $this->insertUpdateData($request, $slug, $dataType->addRows, new $dataType->model_name());
 
-        event(new BreadDataAdded($dataType, $data));
-
-        if (!$request->has('_tagging')) {
-            if (auth()->user()->can('browse', $data)) {
-                $redirect = redirect()->route("voyager.{$dataType->slug}.index");
-            } else {
-                $redirect = redirect()->back();
-            }
-
-            return $redirect->with([
-                    'message'    => __('voyager::generic.successfully_added_new')." {$dataType->getTranslatedAttribute('display_name_singular')}",
-                    'alert-type' => 'success',
-                ]);
-        } else {
-            return response()->json(['success' => true, 'data' => $data]);
+        // event(new BreadDataAdded($dataType, $data));
+        $Newsletter=Newsletter::get();
+        foreach($Newsletter as $News){
+            $email=$News->email;
+            $name=$email;
+            // $email->notify(new \App\Notifications\registerAdministradorLugarCultural());
+            $text=$request["message"];
+            $button="Mas Informacion";
+            $urls="";
+            Mail::to($email)->send(new SendNewsletter($name,$News,$text,$button,$urls));
         }
+        
+        return redirect("/admin/newsletters");
+        // if (!$request->has('_tagging')) {
+        //     if (auth()->user()->can('browse', $data)) {
+        //         $redirect = redirect()->route("voyager.{$dataType->slug}.index");
+        //     } else {
+        //         $redirect = redirect()->back();
+        //     }
+
+        //     return $redirect->with([
+        //             'message'    => __('voyager::generic.successfully_added_new')." {$dataType->getTranslatedAttribute('display_name_singular')}",
+        //             'alert-type' => 'success',
+        //         ]);
+        // } else {
+        //     return response()->json(['success' => true, 'data' => $data]);
+        // }
     }
 
     //***************************************
